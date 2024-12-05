430cbc8a28ac08214ee83637b0bc8aa02ff15f59
Otávio <otavioportella.ads@gmail.com>
2024-12-04 22:13:34 -0300

    ajuste seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,7 +4,7 @@ import bcrypt from 'bcryptjs';
 const db = new PrismaClient();
 
 async function main() {
-  const endereco = await db.endereco.create({
+  const endMatriz = await db.endereco.create({
     data: {
       rua: 'Rua teste',
       numero: '365',
@@ -15,10 +15,44 @@ async function main() {
     },
   });
 
-  const filial = await db.filial.create({
+  const endFilial1 = await db.endereco.create({
+    data: {
+      rua: 'Avenida Brasil',
+      numero: '1250',
+      bairro: 'Jardim Nova York',
+      cep: '16018-310',
+      cidade: 'Araçatuba',
+      estado: 'São Paulo',
+    },
+  });
+
+  const endFilial2 = await db.endereco.create({
+    data: {
+      rua: 'Rua Duque de Caxias',
+      numero: '478',
+      bairro: 'Vila Bandeirantes',
+      cep: '16015-220',
+      cidade: 'Araçatuba',
+      estado: 'São Paulo',
+    },
+  });
+
+  const filialMatriz = await db.filial.create({
     data: {
       nome: 'Matriz',
-      idEndereco: endereco.id,
+      idEndereco: endMatriz.id,
+    },
+  });
+  const filial2 = await db.filial.create({
+    data: {
+      nome: 'Filial 2',
+      idEndereco: endFilial1.id,
+    },
+  });
+  const manutencao = await db.filial.create({
+    data: {
+      nome: 'Manutenção',
+      idEndereco: endFilial2.id,
     },
   });
 
@@ -34,7 +68,7 @@ async function main() {
     },
   });
 
-  await db.perfil.create({
+  const perfilSupervisor = await db.perfil.create({
     data: {
       id: 2,
       nome: 'Supervisor',
@@ -56,7 +90,7 @@ async function main() {
     },
   });
 
-  await db.perfil.create({
+  const perfilMan = await db.perfil.create({
     data: {
       id: 4,
       nome: 'Manutenção',
@@ -67,10 +101,28 @@ async function main() {
     },
   });
 
-  const carteira = await db.carteira.create({
+  const carteiraSuporte = await db.carteira.create({
     data: {
       nome: 'Suporte',
-      idFilial: filial.id,
+      idFilial: filialMatriz.id,
+    },
+  });
+  const carteiraBanco1 = await db.carteira.create({
+    data: {
+      nome: 'Banco 1',
+      idFilial: filialMatriz.id,
+    },
+  });
+  const carteiraCompras = await db.carteira.create({
+    data: {
+      nome: 'Compras',
+      idFilial: manutencao.id,
+    },
+  });
+  await db.carteira.create({
+    data: {
+      nome: 'Carteira 2',
+      idFilial: filial2.id,
     },
   });
 
@@ -80,7 +132,27 @@ async function main() {
       idPerfil: suporte.id,
       matricula: 'admin',
       senha: await bcrypt.hash('admin', 8),
-      idCarteira: carteira.id,
+      idCarteira: carteiraSuporte.id,
+    },
+  });
+
+  await db.user.create({
+    data: {
+      nome: 'Usuário supervisor',
+      idPerfil: perfilSupervisor.id,
+      matricula: 'supervisor',
+      senha: await bcrypt.hash('1234', 8),
+      idCarteira: carteiraBanco1.id,
+    },
+  });
+
+  await db.user.create({
+    data: {
+      nome: 'Usuário compras',
+      idPerfil: perfilMan.id,
+      matricula: 'compras',
+      senha: await bcrypt.hash('1234', 8),
+      idCarteira: carteiraCompras.id,
     },
   });
 }
